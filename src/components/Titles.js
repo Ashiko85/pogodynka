e09b5400d03fc7ecd420729ce09a416b19c8c75d
anna waluszko <ashiko85@gmail.com>
2018-05-20 23:08:51 +0200

    Initial commit - Weather app "Pogodynka".

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>Pogodynka</h1>
+                <p>Sprawdź pogodę w kiedy tylko chcesz!</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
