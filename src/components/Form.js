5cb3acdced51e0f5e63bf47ba6f69fdb5170e797
anna waluszko <ashiko85@gmail.com>
2018-05-21 19:01:53 +0200

    21.05.2018

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,9 @@ import React from "react";
 class Form extends React.Component{
     render(){
         return(
-            <form>
+            <form onSubmit={this.props.getWeather}>
                 <input type="text" name="city" placeholder="Wybierz miasto..." />
-                <input type="text"name="coutry" placeholder="Wybierz kraj..." />
+                <input type="text"name="country" placeholder="Wybierz kraj..." />
                 <button>Sprawdź pogodę</button>
             </form>
         );
